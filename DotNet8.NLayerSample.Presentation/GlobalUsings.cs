0966c413653292ed97845a33b25c14a4631a0ffd
Linn Thit Htoo <lth1212001@gmail.com>
2024-06-16 00:30:51 +0630

    Added Global Usings

---
+++ DotNet8.NLayerSample.Presentation/GlobalUsings.cs
@@ -0,0 +1,6 @@
+// Global using directives
+
+global using DotNet8.NLayerSample.BusinessLogic.Services;
+global using DotNet8.NLayerSample.DataAccess.Services;
+global using DotNet8.NLayerSample.DbService;
+global using Microsoft.EntityFrameworkCore;
