ce51be3570f8cb6db65865b7871076ca587ed2a0
Linn Thit Htoo <lth1212001@gmail.com>
2024-06-16 00:28:24 +0630

    -

--- DotNet8.NLayerSample.Models/Setup/Blog/BlogListResponseModel.cs
+++ DotNet8.NLayerSample.Models/Setup/Blog/BlogListResponseModel.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace DotNet8.NLayerSample.Models.Setup.Blog
+﻿namespace DotNet8.NLayerSample.Models.Setup.Blog
 {
     public class BlogListResponseModel
     {
