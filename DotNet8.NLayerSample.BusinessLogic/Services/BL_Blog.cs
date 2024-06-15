6923d9dfa9ad2e8be3f740683d72b5ac8c3a4752
Linn Thit Htoo <lth1212001@gmail.com>
2024-06-16 00:26:23 +0630

    -

--- DotNet8.NLayerSample.BusinessLogic/Services/BL_Blog.cs
+++ DotNet8.NLayerSample.BusinessLogic/Services/BL_Blog.cs
@@ -1,78 +1,77 @@
 ﻿using DotNet8.NLayerSample.DataAccess.Services;
 using DotNet8.NLayerSample.Models.Setup.Blog;
 
-namespace DotNet8.NLayerSample.BusinessLogic.Services
+namespace DotNet8.NLayerSample.BusinessLogic.Services;
+
+public class BL_Blog
 {
-    public class BL_Blog
+    private readonly DA_Blog _dA_Blog;
+
+    public BL_Blog(DA_Blog dA_Blog)
     {
-        private readonly DA_Blog _dA_Blog;
+        _dA_Blog = dA_Blog;
+    }
 
-        public BL_Blog(DA_Blog dA_Blog)
+    public async Task<BlogListResponseModel> GetBlogs()
+    {
+        try
         {
-            _dA_Blog = dA_Blog;
+            return await _dA_Blog.GetBlogs();
         }
-
-        public async Task<BlogListResponseModel> GetBlogs()
+        catch (Exception ex)
         {
-            try
-            {
-                return await _dA_Blog.GetBlogs();
-            }
-            catch (Exception ex)
-            {
-                throw new Exception(ex.Message);
-            }
+            throw new Exception(ex.Message);
         }
+    }
 
-        public async Task<int> CreateBlog(BlogRequestModel requestModel)
+    public async Task<int> CreateBlog(BlogRequestModel requestModel)
+    {
+        try
         {
-            try
-            {
-                if (string.IsNullOrEmpty(requestModel.BlogTitle))
-                    throw new Exception("Blog Title cannot be empty.");
+            if (string.IsNullOrEmpty(requestModel.BlogTitle))
+                throw new Exception("Blog Title cannot be empty.");
 
-                if (string.IsNullOrEmpty(requestModel.BlogAuthor))
-                    throw new Exception("Blog Author cannot be empty.");
+            if (string.IsNullOrEmpty(requestModel.BlogAuthor))
+                throw new Exception("Blog Author cannot be empty.");
 
-                if (string.IsNullOrEmpty(requestModel.BlogContent))
-                    throw new Exception("Blog Content cannot be empty.");
+            if (string.IsNullOrEmpty(requestModel.BlogContent))
+                throw new Exception("Blog Content cannot be empty.");
 
-                return await _dA_Blog.CreateBlog(requestModel);
-            }
-            catch (Exception ex)
-            {
-                throw new Exception(ex.Message);
-            }
+            return await _dA_Blog.CreateBlog(requestModel);
         }
-
-        public async Task<int> PatchBlog(BlogRequestModel requestModel, int id)
+        catch (Exception ex)
         {
-            try
-            {
-                if (id <= 0)
-                    throw new Exception("Id is invalid.");
-
-                return await _dA_Blog.PatchBlog(requestModel, id);
-            }
-            catch (Exception ex)
-            {
-                throw new Exception(ex.Message);
-            }
+            throw new Exception(ex.Message);
         }
+    }
 
-        public async Task<int> DeleteBlog(int id)
+    public async Task<int> PatchBlog(BlogRequestModel requestModel, int id)
+    {
+        try
         {
-            try
-            {
-                if (id <= 0)
-                    throw new Exception("Id is invalid.");
+            if (id <= 0)
+                throw new Exception("Id is invalid.");
 
-                return await _dA_Blog.DeleteBlog(id);
-            }
-            catch (Exception ex)
-            {
-                throw new Exception(ex.Message);
-            }
+            return await _dA_Blog.PatchBlog(requestModel, id);
+        }
+        catch (Exception ex)
+        {
+            throw new Exception(ex.Message);
+        }
+    }
+
+    public async Task<int> DeleteBlog(int id)
+    {
+        try
+        {
+            if (id <= 0)
+                throw new Exception("Id is invalid.");
+
+            return await _dA_Blog.DeleteBlog(id);
+        }
+        catch (Exception ex)
+        {
+            throw new Exception(ex.Message);
         }
     }
 }
