90358c819996da6689ef4e1fd15524dace43425a
Linn Thit Htoo <lth1212001@gmail.com>
2024-06-16 00:26:38 +0630

    -

--- DotNet8.NLayerSample.DataAccess/Services/DA_Blog.cs
+++ DotNet8.NLayerSample.DataAccess/Services/DA_Blog.cs
@@ -2,12 +2,6 @@
 using DotNet8.NLayerSample.Mapper;
 using DotNet8.NLayerSample.Models.Setup.Blog;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.EntityFrameworkCore.Metadata.Conventions;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace DotNet8.NLayerSample.DataAccess.Services
 {
