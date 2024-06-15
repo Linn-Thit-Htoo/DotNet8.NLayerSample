798cb67307cea46e52ea84a968c6b24d0c2929d4
Linn Thit Htoo <lth1212001@gmail.com>
2024-06-16 00:33:51 +0630

    -

--- DotNet8.NLayerSample.DbService/AppDbContext.cs
+++ DotNet8.NLayerSample.DbService/AppDbContext.cs
@@ -5,9 +5,8 @@ namespace DotNet8.NLayerSample.DbService;
 
 public class AppDbContext : DbContext
 {
-    public AppDbContext(DbContextOptions options) : base(options)
-    {
-    }
+    public AppDbContext(DbContextOptions options)
+        : base(options) { }
 
     public DbSet<Tbl_Blog> Blogs { get; set; }
 }
