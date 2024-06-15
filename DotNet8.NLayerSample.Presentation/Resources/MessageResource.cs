21e3f0f44ab25f1d0b863dea65cfb382592767a9
Linn Thit Htoo <lth1212001@gmail.com>
2024-06-16 00:29:34 +0630

    -

--- DotNet8.NLayerSample.Presentation/Resources/MessageResource.cs
+++ DotNet8.NLayerSample.Presentation/Resources/MessageResource.cs
@@ -1,10 +1,9 @@
-﻿namespace DotNet8.NLayerSample.Presentation.Resources
+﻿namespace DotNet8.NLayerSample.Presentation.Resources;
+
+public static class MessageResource
 {
-    public static class MessageResource
-    {
-        public static string SaveSuccess { get; } = "Saving Successful.";
-        public static string SaveFail { get; } = "Saving Fail.";
-        public static string DeleteSuccess { get; } = "Deleting Successful.";
-        public static string DeleteFail { get; } = "Deleting Fail.";
-    }
+    public static string SaveSuccess { get; } = "Saving Successful.";
+    public static string SaveFail { get; } = "Saving Fail.";
+    public static string DeleteSuccess { get; } = "Deleting Successful.";
+    public static string DeleteFail { get; } = "Deleting Fail.";
 }
