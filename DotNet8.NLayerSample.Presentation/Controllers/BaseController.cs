b7b19c2c5d9ba71bb427556b642345fe036d6ce3
Linn Thit Htoo <lth1212001@gmail.com>
2024-06-16 00:29:10 +0630

    -

--- DotNet8.NLayerSample.Presentation/Controllers/BaseController.cs
+++ DotNet8.NLayerSample.Presentation/Controllers/BaseController.cs
@@ -2,30 +2,29 @@
 using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json;
 
-namespace DotNet8.NLayerSample.Presentation.Controllers
+namespace DotNet8.NLayerSample.Presentation.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class BaseController : ControllerBase
 {
-    [Route("api/[controller]")]
-    [ApiController]
-    public class BaseController : ControllerBase
+    protected IActionResult Content(object? obj)
     {
-        protected IActionResult Content(object? obj)
-        {
-            return StatusCode(200, JsonConvert.SerializeObject(obj));
-        }
+        return StatusCode(200, JsonConvert.SerializeObject(obj));
+    }
 
-        protected IActionResult Created()
-        {
-            return StatusCode(201, MessageResource.SaveSuccess);
-        }
+    protected IActionResult Created()
+    {
+        return StatusCode(201, MessageResource.SaveSuccess);
+    }
 
-        protected IActionResult Updated()
-        {
-            return StatusCode(202, MessageResource.SaveSuccess);
-        }
+    protected IActionResult Updated()
+    {
+        return StatusCode(202, MessageResource.SaveSuccess);
+    }
 
-        protected IActionResult Deleted()
-        {
-            return StatusCode(202, MessageResource.DeleteSuccess);
-        }
+    protected IActionResult Deleted()
+    {
+        return StatusCode(202, MessageResource.DeleteSuccess);
     }
 }
