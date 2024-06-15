83641220fd4f35705add9da7db5913754d5fe443
Linn Thit Htoo <lth1212001@gmail.com>
2024-06-16 00:28:15 +0630

    -

--- DotNet8.NLayerSample.Mapper/ChangeModel.cs
+++ DotNet8.NLayerSample.Mapper/ChangeModel.cs
@@ -1,29 +1,28 @@
 ﻿using DotNet8.NLayerSample.DbService.Entities;
 using DotNet8.NLayerSample.Models.Setup.Blog;
 
-namespace DotNet8.NLayerSample.Mapper
-{
-    public static class ChangeModel
-    {
-        public static BlogModel Change(this Tbl_Blog dataModel)
-        {
-            return new BlogModel
-            {
-                BlogId = dataModel.BlogId,
-                BlogTitle = dataModel.BlogTitle,
-                BlogAuthor = dataModel.BlogAuthor,
-                BlogContent = dataModel.BlogContent
-            };
-        }
+namespace DotNet8.NLayerSample.Mapper;
 
-        public static Tbl_Blog Change(this BlogRequestModel requestModel)
+public static class ChangeModel
+{
+    public static BlogModel Change(this Tbl_Blog dataModel)
+    {
+        return new BlogModel
         {
-            return new Tbl_Blog
-            {
-                BlogTitle = requestModel.BlogTitle,
-                BlogAuthor = requestModel.BlogAuthor,
-                BlogContent = requestModel.BlogContent
-            };
-        }
+            BlogId = dataModel.BlogId,
+            BlogTitle = dataModel.BlogTitle,
+            BlogAuthor = dataModel.BlogAuthor,
+            BlogContent = dataModel.BlogContent
+        };
+    }
+
+    public static Tbl_Blog Change(this BlogRequestModel requestModel)
+    {
+        return new Tbl_Blog
+        {
+            BlogTitle = requestModel.BlogTitle,
+            BlogAuthor = requestModel.BlogAuthor,
+            BlogContent = requestModel.BlogContent
+        };
     }
 }
