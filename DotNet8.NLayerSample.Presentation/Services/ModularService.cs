e14031a26b8104ab1ace17f7f1332d33b62a1970
Linn Thit Htoo <lth1212001@gmail.com>
2024-06-16 00:29:45 +0630

    -

--- DotNet8.NLayerSample.Presentation/Services/ModularService.cs
+++ DotNet8.NLayerSample.Presentation/Services/ModularService.cs
@@ -3,61 +3,60 @@ using DotNet8.NLayerSample.DataAccess.Services;
 using DotNet8.NLayerSample.DbService;
 using Microsoft.EntityFrameworkCore;
 
-namespace DotNet8.NLayerSample.Presentation.Services
+namespace DotNet8.NLayerSample.Presentation.Services;
+
+public static class ModularService
 {
-    public static class ModularService
+    public static IServiceCollection AddServices(
+        this IServiceCollection services,
+        WebApplicationBuilder builder
+    )
     {
-        public static IServiceCollection AddServices(
-            this IServiceCollection services,
-            WebApplicationBuilder builder
-        )
-        {
-            services
-                .AddDbContextService(builder)
-                .AddBusinessLogicServices()
-                .AddDataAccessServices()
-                .AddJsonServices();
+        services
+            .AddDbContextService(builder)
+            .AddBusinessLogicServices()
+            .AddDataAccessServices()
+            .AddJsonServices();
 
-            return services;
-        }
+        return services;
+    }
 
-        private static IServiceCollection AddDbContextService(
-            this IServiceCollection services,
-            WebApplicationBuilder builder
-        )
-        {
-            builder.Services.AddDbContext<AppDbContext>(
-                opt =>
-                {
-                    opt.UseSqlServer(builder.Configuration.GetConnectionString("DbConnection"));
-                },
-                ServiceLifetime.Transient
-            );
+    private static IServiceCollection AddDbContextService(
+        this IServiceCollection services,
+        WebApplicationBuilder builder
+    )
+    {
+        builder.Services.AddDbContext<AppDbContext>(
+            opt =>
+            {
+                opt.UseSqlServer(builder.Configuration.GetConnectionString("DbConnection"));
+            },
+            ServiceLifetime.Transient
+        );
 
-            return services;
-        }
+        return services;
+    }
 
-        private static IServiceCollection AddBusinessLogicServices(this IServiceCollection services)
-        {
-            services.AddScoped<BL_Blog>();
-            return services;
-        }
+    private static IServiceCollection AddBusinessLogicServices(this IServiceCollection services)
+    {
+        services.AddScoped<BL_Blog>();
+        return services;
+    }
 
-        private static IServiceCollection AddDataAccessServices(this IServiceCollection services)
-        {
-            services.AddScoped<DA_Blog>();
-            return services;
-        }
+    private static IServiceCollection AddDataAccessServices(this IServiceCollection services)
+    {
+        services.AddScoped<DA_Blog>();
+        return services;
+    }
 
-        private static IServiceCollection AddJsonServices(this IServiceCollection services)
-        {
-            services
-                .AddControllers()
-                .AddJsonOptions(opt =>
-                {
-                    opt.JsonSerializerOptions.PropertyNamingPolicy = null;
-                });
-            return services;
-        }
+    private static IServiceCollection AddJsonServices(this IServiceCollection services)
+    {
+        services
+            .AddControllers()
+            .AddJsonOptions(opt =>
+            {
+                opt.JsonSerializerOptions.PropertyNamingPolicy = null;
+            });
+        return services;
     }
 }
